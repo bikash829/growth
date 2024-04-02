0db1012552b4e37ae8f9192a5fb70a50fa8b2285
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-04-02 10:57:15 +0600

    marquee effect added to logo on nav bar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -35,3 +35,28 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".testimonial-container").addClass("moveRightToLeft");
 });
+
+// scroll marquee animation
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    // console.log(scrollerContent);
+
+    scrollerContent.forEach((content) => {
+      const duplicatedItem = content.cloneNode(true);
+      //   scrollerInner.appendChild(clone);
+      duplicatedItem.setAttribute("area-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+      console.log(duplicatedItem);
+    });
+  });
+}
