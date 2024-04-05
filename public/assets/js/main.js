ca4fa4e558507b11c8fe86c720df770954b3d151
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-04-06 00:15:40 +0600

    completed except header

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -109,7 +109,9 @@ $(document).ready(function () {
 });
 
 // aos initialized
-AOS.init();
+AOS.init({
+  once: true,
+});
 
 // AOS.init({
 //   // Global settings:
@@ -127,7 +129,7 @@ AOS.init();
 //   delay: 0, // values from 0 to 3000, with step 50ms
 //   duration: 400, // values from 0 to 3000, with step 50ms
 //   easing: 'ease', // default easing for AOS animations
-//   once: false, // whether animation should happen only once - while scrolling down
+// once: false, // whether animation should happen only once - while scrolling down
 //   mirror: false, // whether elements should animate out while scrolling past them
 //   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
